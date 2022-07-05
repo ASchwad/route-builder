fc9b0ed9aba66cdec1fff418cefafc2958a903cf
ASchwad <a.schoenenwald@gmx.de>
2022-07-05 22:56:57 +0200

    Outsource calculateCrowDistance

--- src/components/Controls/TotalDistance.tsx
+++ src/components/Controls/TotalDistance.tsx
@@ -1,32 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { ICoordinate } from '../../App';
-
-function crowDistance(coordinates1: ICoordinate, coordinates2: ICoordinate) {
-    let { lat: lat1, long: lon1 } = coordinates1
-    let { lat: lat2, long: lon2 } = coordinates2
-    if ((lat1 === lat2) && (lon1 === lon2)) {
-        return 0;
-    }
-    else {
-        var radlat1 = Math.PI * lat1 / 180;
-        var radlat2 = Math.PI * lat2 / 180;
-        var theta = lon1 - lon2;
-        var radtheta = Math.PI * theta / 180;
-        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        if (dist > 1) {
-            dist = 1;
-        }
-        dist = Math.acos(dist);
-        dist = dist * 180 / Math.PI;
-        dist = dist * 60 * 1.1515;
-        dist = dist * 1.609344
-
-        return dist;
-    }
-}
+import { crowDistance } from '../../utils/calculateCrowDistance';
 
 function TotalDistance({ waypoints }: { waypoints: ICoordinate[] }) {
-
     const [totalDistance, setTotalDistance] = useState(0);
 
     useEffect(() => {
@@ -37,7 +13,6 @@ function TotalDistance({ waypoints }: { waypoints: ICoordinate[] }) {
             return acc + crowDistance(waypoints[index], waypoints[index + 1]);
         }, 0))
     }, [waypoints]);
-
 
     return (
         <p style={{ color: "#fff" }}>
